b427af513f00ba49e7b5c7eab00284b11b916311
mandu <jys0972@gmail.com>
2024-09-26 19:44:56 +0900

    test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("ㅎㅏㅇㅣ");
     }
 
     // Update is called once per frame
