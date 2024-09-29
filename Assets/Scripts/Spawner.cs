2c41b4d2b729aed7c9998674e7a2df359b926e47
191012 <kimhj191012@naver.com>
2024-09-29 15:39:59 +0900

    feat(Player.cs, Spawner.cs)
    플레이어 이동 및 원 랜덤 생성 구현

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject Circle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
