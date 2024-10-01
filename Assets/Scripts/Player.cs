256e3a7f601307a81ce92fe289e265a15c510a83
reductant01 <hwj07280728@gmail.com>
2024-10-02 00:36:05 +0900

    unity기초

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    float moveSpeed = 2;
+
+    // 플레이어 이동 기능
+    // 1. 키 입력을 받는다.
+    // 2. 키 입력에 따라서, 플레이어 오브젝트를 이동시킨다.
+    private void Update() // 매 프레임마다 동작을 한다. 컴퓨터나 동작하는 환경의 성능에 따라서 1초당 몇 프레임인지가 달라짐.
     {
-        
+        // Input.GetKeyDown()
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("Press W!");
+           transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+           Debug.Log("Press A!");
+           transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+           Debug.Log("Press S!");
+           transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+           Debug.Log("Press D!");
+           transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+
+        // Input.GetAxis()
+        transform.position += Vector3.right * Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        transform.position += Vector3.up * Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
     }
 }
