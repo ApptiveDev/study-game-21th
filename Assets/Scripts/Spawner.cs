1d18bf719027873d45c84e216eca2122b8a4a415
snorlax914 <whisu2000@gmail.com>
2024-10-05 22:53:26 +0900

    feat(Spawner.cs, Enemy.cs)
    Player를 따라오는 Enemy를 1초 마다 생성하는 Spawner 구현

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,29 +5,47 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] GameObject circleObject;
+    float curTime = 0;
+    int num = 0;
     // Start is called before the first frame update
+
     void Start()
     {
-        for(int i=0; i<10; i++)
-        {
-            float x = Random.Range(-3f, 3f);
-            float y = Random.Range(-3f, 3f);
-            
-            Vector3 newPosition = new Vector3(x, y, 0);
-            float r = Random.Range(0, 1f);
-            float g = Random.Range(0, 1f);
-            float b = Random.Range(0, 1f);
-
-            Color newColor = new Color(r, g, b);
-
-            Instantiate(circleObject, newPosition, Quaternion.identity);
-            circleObject.GetComponent<SpriteRenderer>().color = newColor;
-        }
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        curTime += Time.deltaTime;
+        if(curTime >= 1 && num <10)
+        {
+            MakeRandomEnemy();
+            curTime = 0;
+            num++;
+        }
         
+    }
+    void MakeRandomEnemy()
+    {
+        Instantiate(circleObject);
+        circleObject.transform.position = PickRandomPosition();
+        circleObject.GetComponent<SpriteRenderer>().color = PickRandomColor();
+    }
+    Vector3 PickRandomPosition()
+    {
+        float x = Random.Range(-3f, 3f);
+        float y = Random.Range(-3f, 3f);
+
+        return new Vector3(x, y, 0);
+    }
+
+    Color PickRandomColor()
+    {
+        float r = Random.Range(0, 1f);
+        float g = Random.Range(0, 1f);
+        float b = Random.Range(0, 1f);
+
+        return new Color(r, g, b);
     }
 }
