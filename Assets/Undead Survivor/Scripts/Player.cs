5d2cc1b7d3b517bac21cb66e4ce8a716382b55df
snorlax914 <whisu2000@gmail.com>
2024-11-02 22:56:27 +0900

    feat(Player.cs)
    Player의  이동과 애니메이션 구현

---
+++ Assets/Undead Survivor/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    SpriteRenderer renderer;
+    Rigidbody2D rigid;
+    Animator animator;
+    public float speed;
+    public Vector2 inputVector;
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        renderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        inputVector.x = Input.GetAxisRaw("Horizontal");
+        inputVector.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVector = inputVector.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVector);
+    }
+    private void LateUpdate()
+    {
+        animator.SetFloat("Speed", inputVector.magnitude);
+        if(inputVector.x != 0)
+        {
+            renderer.flipX = inputVector.x < 0;
+        }
+    }
+}
